1c1e2e1d95e3e051125f055d745db911ad81d40c
Ben-Kips <benyegon25@gmail.com>
2023-10-20 02:03:58 +0300

    first commt

---
+++ controllers/contactController.js
@@ -0,0 +1,75 @@
+ const asyncHandler = require ("express-async-handler");
+const Contact = require ("../models/contactModel")
+
+//@desc get all contacts
+//@route GET /api/contacts
+//@access public
+
+const getContacts =asyncHandler(async (req,res) =>{
+    const contacts = await  Contact.find();
+    res.status(200).json(contacts);
+});
+ 
+//@desc Create new  contacts
+//@route POST /api/contacts
+//@access public
+
+const createContact =asyncHandler( async(req,res) =>{
+    console.log("The request body is :",req.body)
+    const{name,email,phone}=req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+      const contact = await Contact.create({
+        name,
+        email,
+        phone,
+      }); 
+    res.status(201).json(contact )
+});
+ 
+//@desc Get contact
+//@route POST /api/contacts/:id
+//@access public
+
+const getContact =asyncHandler( async(req,res) =>{
+    const contact =await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact Not Found ");
+    };
+
+    res.status(200).json(contact)
+});
+//@desc Delete contact
+//@route delete /api/contacts/:id
+//@access public
+
+const deleteContact = asyncHandler(async(req,res) =>{
+    const contact =await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact Not Found ");
+    }
+     await Contact.remove();
+    res.status(200).json(contact)
+});
+//@desc update contact 
+//@route Put /api/contacts/:id
+//@access public
+
+const updateContact = asyncHandler(async(req,res) =>{
+    const contact =await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact Not Found ");
+    };
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new :true}
+    );
+    res.status(200).json(updatedContact)
+});
+module.exports = {getContacts,createContact,getContact,deleteContact,updateContact};
